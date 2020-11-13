a24f68a0c1904bba12e683704550404dd9d492b3
Juan Gallardo <juan.gallardo.ab@gmail.com>
2020-11-12 18:30:42 -0700

    Add W9B code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// W9A
+
 var age = 18;
 var is_subscribed = true;
 var result = "";
@@ -9,3 +11,22 @@ result += "The user is " +
     " subscribed";
 
 console.log(result);
+
+// W9B
+
+var ages = [1, 18, 20, 25, 10, 17.5, 18.2];
+var subscriptions = [true, true, false, true, false, true, false];
+
+if (ages.length != subscriptions.length) {
+    throw "The two arrays must have the same length!";
+}
+
+ages.forEach((age_at_i, index) => {
+    var result = "";
+    result += "The user is " +
+        (age_at_i < 18 ? "younger than 18" : "18 and older") +
+        " and " +
+        (subscriptions[index] ? "is" : "not") +
+        " subscribed";
+    console.log(result);
+});
